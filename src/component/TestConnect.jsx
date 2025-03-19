ccd39097fe0c395a9127af832531f889f227d602
7dong7 <tmdkdl777@gmail.com>
2025-03-19 22:49:31 +0900

    모든 요청에 대한 access token Header 전달 (useContext, interceptor, axios)2

--- src/component/TestConnect.jsx
+++ src/component/TestConnect.jsx
@@ -32,36 +32,36 @@ const TestConnect = () => {
         <div className={"TextConnect"}>
             {/* 1. 테스트 연결 확인 */}
             <GetButton text={"(단순) String 테스트"} url={"/api/text"} api={api}/>
-            {/*/!* 2. dto 응답 테스트 *!/*/}
-            {/*<Button text={"(단순) dto 응답 테스트"} url={"/api/dto"} api={api}/>*/}
-            {/*/!* 3. map 응답 테스트 *!/*/}
-            {/*<Button text={"(단순) map 응답 테스트"} url={"/api/map"} api={api}/>*/}
+            {/* 2. dto 응답 테스트 */}
+            <GetButton text={"(단순) dto 응답 테스트"} url={"/api/dto"} api={api}/>
+            {/* 3. map 응답 테스트 */}
+            <GetButton text={"(단순) map 응답 테스트"} url={"/api/map"} api={api}/>
 
-            {/*<br/>*/}
+            <br/>
 
-            {/*/!* 4. 상태변환 String 응답 테스트 *!/*/}
-            {/*<div className={"status_request"}>*/}
-            {/*    <Button text={"(Ok 응답) String 응답 테스트"} url={"/api/statusTextOk"} api={api}/>*/}
-            {/*    <Button text={"(Bad 응답) String 응답 테스트"} url={"/api/statusTextBad"} api={api}/>*/}
-            {/*</div>*/}
+            {/* 4. 상태변환 String 응답 테스트 */}
+            <div className={"status_request"}>
+                <GetButton text={"(Ok 응답) String 응답 테스트"} url={"/api/statusTextOk"} api={api}/>
+                <GetButton text={"(Bad 응답) String 응답 테스트"} url={"/api/statusTextBad"} api={api}/>
+            </div>
 
-            {/*/!* 5. 상태변환 dto 응답 테스트 *!/*/}
-            {/*<div className={"status_request"}>*/}
-            {/*    <Button text={"(Ok 응답) dto 응답 테스트"} url={"/api/statusDtoOk"} api={api}/>*/}
-            {/*    <Button text={"(Bad 응답) dto 응답 테스트"} url={"/api/statusDtoBad"} api={api}/>*/}
-            {/*</div>*/}
+            {/* 5. 상태변환 dto 응답 테스트 */}
+            <div className={"status_request"}>
+                <GetButton text={"(Ok 응답) dto 응답 테스트"} url={"/api/statusDtoOk"} api={api}/>
+                <GetButton text={"(Bad 응답) dto 응답 테스트"} url={"/api/statusDtoBad"} api={api}/>
+            </div>
 
-            {/*/!* 6. 상태변환 dto 응답 테스트 *!/*/}
-            {/*<div className={"status_request"}>*/}
-            {/*    <Button text={"(Ok 응답) map 응답 테스트"} url={"/api/statusMapOk"} api={api}/>*/}
-            {/*    <Button text={"(Bad 응답) map 응답 테스트"} url={"/api/statusMapBad"} api={api}/>*/}
-            {/*</div>*/}
+            {/* 6. 상태변환 dto 응답 테스트 */}
+            <div className={"status_request"}>
+                <GetButton text={"(Ok 응답) map 응답 테스트"} url={"/api/statusMapOk"} api={api}/>
+                <GetButton text={"(Bad 응답) map 응답 테스트"} url={"/api/statusMapBad"} api={api}/>
+            </div>
 
-            {/*<br/>*/}
-            {/*<div className={"status_request"}>*/}
-            {/*    /!* 7. JSON 데이터를 백으로 보내기 테스트*!/*/}
-            {/*    <RequestButton text={"백으로 데이터 보내기"} url={"/api/requestData"} api={api}/>*/}
-            {/*</div>*/}
+            <br/>
+            <div className={"status_request"}>
+                {/* 7. JSON 데이터를 백으로 보내기 테스트*/}
+                <RequestButton text={"백으로 데이터 보내기"} url={"/api/requestData"} api={api}/>
+            </div>
 
             <br/>
             <div className={"status_request"}>
